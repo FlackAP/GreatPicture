7280ef25c0d7628fc3a400f3b37aad39ce303369
FlackAP <flack.ap@gmail.com>
2014-04-24 11:59:50 -0400

    added storyboards and cool camera roll-accessing functionality

--- Frame It/APFViewController.h
+++ Frame It/APFViewController.h
@@ -8,6 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface APFViewController : UIViewController
+@interface APFViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)sendPhoto:(UIButton*)sender;
 
 @end
--- Frame It/APFViewController.m
+++ Frame It/APFViewController.m
@@ -8,22 +8,50 @@
 
 #import "APFViewController.h"
 
-@interface APFViewController ()
-
-@end
 
 @implementation APFViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
+}
+
+- (IBAction)sendPhoto:(UIButton *)sender {
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    
+    
+}
+
+#pragma mark - Image Picker Controller delegate methods
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = chosenImage;
+
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
 }
 
 @end
